3e8d098b115d07a5372dc39ad4debba1e39fa9fd
bharadwaj-raju <bharadwaj.raju@keemail.me>
2016-09-30 20:18:05 +0530

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,8 +34,7 @@
 import sys
 import os
 
-sys.path.insert(0, '..')
-sys.path.insert(0, '.')
+sys.path.insert(0, os.path.pardir)
 
 extensions = [
     'sphinx.ext.autodoc',
@@ -138,7 +137,9 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+if not os.getenv('READTHEDOCS'):
+	html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
