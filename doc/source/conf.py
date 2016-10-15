9ef7853985b4aed767771d0fd781d90a1608f2a8
bharadwaj-raju <bharadwaj.raju@keemail.me>
2016-10-15 17:01:01 +0530

    Fix doc builds

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,6 +39,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))))
 
 print(sys.path)
 
